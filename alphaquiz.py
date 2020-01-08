a822e0db00a16fcc33accac595f8149bda77d96c
huntagain2008 <huntagain@gmail.com>
2020-01-08 11:04:37 +0800

    add comment alphaquiz.py

--- alphaquiz.py
+++ alphaquiz.py
@@ -4,16 +4,19 @@
 import time
 import random
 
-ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# Set up the constants
 QUIZ_SIZE = 3
 QUIZ_DURATION = 30
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def slowprint(text, duration):
     for char in text:
-        print(char, flush=True, end='')
-        time.sleep(duration)
+        # Set flush=True here so the text is immediately printed
+        print(char, flush=True, end='') # end='' means no newline
+        time.sleep(duration) # Print a newline
     print()
-        
+
+# Fancy animation for the title:
 slowprint(ALPHABET, 0.02)
 slowprint('QUIZ'.center(len(ALPHABET)), 0.02)
 slowprint(''.join(sorted(ALPHABET, reverse=True)), 0.02)
@@ -25,22 +28,32 @@ For example:
    sort these'''.format(QUIZ_DURATION))
 
 print('press Enter to start:')
-input()
-startTime = time.time()
+input() # Let the player press Enter to start the game
 
-while True:
+startTime = time.time() # Get the current time for the start time
+numCorrect = 0 # Number of questions answered correctly
+while True: # Main game loop
+    # Come up with QUESTION_SIZE letters for the question:
     quiz = random.sample(ALPHABET, QUIZ_SIZE)
     print(' '.join(quiz))
     print()
     response = input('>').upper()
-    if time.time() - startTime >= QUIZ_DURATION:
+
+    # Check if the quiz's time is up:
+    if time.time() - startTime > QUIZ_DURATION:
         print('TIME IS UP!')
         break
 
-    quiz.sort()
-    if list(response) == quiz:
+    # Check if the response is correct:
+    if list(response) == sorted(quiz):
         print('Correct!')
+        numCorrect += 1 # Increase the score by 1
     else:
         print('Wrong!')
+    # At this point, go back to the start of the main game loop.
+
+# After the loop exits, the quiz is over, Show the final score:
+print('In {} seconds you got {} correct!'.format(QUIZ_DURATION, numCorrect))
+print('Thank you playing!')
         
     
